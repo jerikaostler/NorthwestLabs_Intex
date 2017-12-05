afb480a0a77bfd6ddda35cc308ec44fb3f8a32f2
joshblotter <joshblotter@gmail.com>
2017-12-05 15:51:57 -0700

    Models?

---
+++ NorthwestLabs/Models/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    [Table("Equipment")]
+    public class Equipment
+    {
+        [Key]
+        public int EquipmentID;
+
+        [Display(Name = "Equipment Description")]
+        public string Description;
+    }
+}
---
+++ NorthwestLabs/Models/Material.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    [Table("Material")]
+    public class Material
+    {
+        [Key]
+        public int MaterialID;
+
+        [Display(Name = "Material Description")]
+        public string Description;
+
+        [Display(Name = "Material Cost")]
+        public decimal MaterialCost;
+
+    }
+}
